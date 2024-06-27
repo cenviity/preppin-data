7e1c1b8da8e5a2b7ac0ea8644fc3a1b8cdd52fe6
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-06-27 16:59:48 +1000

    Remove Jupyter-specific code

--- 2024/01/pd_2024_01.py
+++ 2024/01/pd_2024_01.py
@@ -1,38 +1,27 @@
-# %% [markdown]
-# # 2024 week 1: Prep Air's Flow Card
+"""2024 week 1: Prep Air's Flow Card
 
-# %% [markdown]
-# https://preppindata.blogspot.com/2024/01/2024-week-1-prep-airs-flow-card.html
+<https://preppindata.blogspot.com/2024/01/2024-week-1-prep-airs-flow-card.html>
+"""
 
-# %% [markdown]
-# ## Solution
-
-# %%
 import pandas as pd
 
 pd.options.mode.copy_on_write = True
 
-# %%
 df = pd.read_csv("data/input.csv")
 df.head()
 
-# %%
 # Split the `Flight Details` field to form new fields
 split_data = df["Flight Details"].str.split("//", expand=True)
 split_data.columns = ["Date", "Flight Number", "From-To", "Class", "Price"]
 
 df = df.assign(**split_data)
-df
 
-# %%
 # Split the `From-To` field as well
 split_data = df["From-To"].str.split("-", expand=True)
 split_data.columns = ["From", "To"]
 
 df = df.assign(**split_data)
-df
 
-# %%
 # Remove and reorder columns
 columns = [
     "Date",
@@ -46,12 +35,7 @@ columns = [
     "Meal Type",
 ]
 df = df[columns]
-df
 
-# %%
-df.dtypes
-
-# %%
 # Cast to appropriate data types
 df = df.astype(
     {
@@ -59,48 +43,31 @@ df = df.astype(
         "Price": float,
     }
 )
-df.dtypes
 
-# %%
 # Check `Flow Card?` field only has values of 0 or 1
 df["Flow Card?"].isin([0, 1]).all()
 
-# %%
 # Re-encode `Flow Card?` field as "Yes" or "No"
 df = df.replace({"Flow Card?": {0: "No", 1: "Yes"}})
-df
 
-# %%
 # Separate into groups with and without Flow Card
 flow_card_groups = df.groupby(by="Flow Card?")
 
 flow_card_yes = flow_card_groups.get_group("Yes").reset_index(drop=True)
-flow_card_yes
 
-# %%
 flow_card_no = flow_card_groups.get_group("No").reset_index(drop=True)
-flow_card_no
 
-# %% [markdown]
-# ## Tests
-
-# %%
+# Tests
 import pandas.testing as pdt
 
-# %%
 expected_flow_card_yes = pd.read_csv(
     "data/output_flow_card_yes.csv", parse_dates=["Date"], date_format="%d/%m/%Y"
 )
-expected_flow_card_yes
 
-# %%
 expected_flow_card_no = pd.read_csv(
     "data/output_flow_card_no.csv", parse_dates=["Date"], date_format="%d/%m/%Y"
 )
-expected_flow_card_no
 
-# %%
 pdt.assert_frame_equal(expected_flow_card_yes, flow_card_yes)
 
-# %%
 pdt.assert_frame_equal(expected_flow_card_no, flow_card_no)
